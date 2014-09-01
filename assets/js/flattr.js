a73132d09d7850d919c69ecd51cff0b3f0f34152
Michael Kühnel <m.kuehnel@micromata.de>
2014-09-01 07:45:00 +0200

    Add flattr for post the right way

---
+++ assets/js/flattr.js
@@ -0,0 +1,13 @@
+(function() {
+    var s = document.createElement('script');
+    var t = document.getElementsByTagName('script')[0];
+
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//api.flattr.com/js/0.6/load.js?mode=auto' +
+    		'&uid=mischah' +
+    		'&button=compact' +
+    		'&html5-key-prefix=data-flattr';
+
+    t.parentNode.insertBefore(s, t);
+ })();
